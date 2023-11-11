327bf10a2ca7a051fd04ab9a84d9ad12a917b502
Your Name <carpentierodaniele@gmail.com>
2023-11-11 16:56:53 +0100

    fix: comments

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+/* Assegnazione variabili */
 const images = ['01.jpg', '02.jpg', '03.jpg','04.jpg','05.jpg']
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const items = document.querySelector(".items");
+/* immagine visualizzata corrente */
 let currentImage = 0;
 
-
+/* inizializzazione ciclo for per le immagini */
 for (let i = 0; i < images.length; i++){
     const item = document.createElement("div");
     item.classList.add('item');
@@ -21,8 +23,10 @@ for (let i = 0; i < images.length; i++){
     items.append(item);
 }
 
+/* selezioni tutte le classi che contengono le immagini */
 const itemsAll = document.querySelectorAll('.item');
 
+/* al click su prev visualizza l'immagine precedente */
 prev.addEventListener('click', function (){
     console.log("ho cliccato su prev");
     if(currentImage > 0){
@@ -32,6 +36,7 @@ prev.addEventListener('click', function (){
     }
 });
 
+/* al click su next visualizza l'immagine successiva */
 next.addEventListener ('click', function (){
     console.log("ho cliccato su next");
     if(currentImage < itemsAll.length - 1){

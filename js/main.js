b39061f51eb1b7f40f71d37c0073f5779e9a2142
Daniele Carpentiero <carpentierodaniele@gmail.com>
2023-11-12 10:31:32 +0100

    fix: bonus 1 per ciclo immagine infinita

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 /* Assegnazione variabili */
-const images = ['01.jpg', '02.jpg', '03.jpg','04.jpg','05.jpg']
+const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const items = document.querySelector(".items");
-/* immagine visualizzata corrente */
 let currentImage = 0;
 
 /* inizializzazione ciclo for per le immagini */
-for (let i = 0; i < images.length; i++){ 
+for (let i = 0; i < images.length; i++) {
     const item = document.createElement("div");
     item.classList.add('item');
-    
-    if (currentImage === i){
-        item.classList.add('active')
-    }
 
     const img = document.createElement('img');
     img.src = `img/${images[i]}`;
@@ -21,27 +16,27 @@ for (let i = 0; i < images.length; i++){
 
     item.append(img)
     items.append(item);
+
+    if (currentImage === i) {
+        item.classList.add('active');
+    }
 }
 
 /* seleziono tutte le classi che contengono le immagini */
 const itemsAll = document.querySelectorAll('.item');
 
 /* al click su prev visualizza l'immagine precedente */
-prev.addEventListener('click', function (){
+prev.addEventListener('click', function () {
     console.log("ho cliccato su prev");
-    if(currentImage > 0){
-        itemsAll[currentImage].classList.remove('active');
-        currentImage--;
-        itemsAll[currentImage].classList.add('active');
-    }
+    itemsAll[currentImage].classList.remove('active');
+    currentImage = (currentImage - 1 + itemsAll.length) % itemsAll.length;
+    itemsAll[currentImage].classList.add('active');
 });
 
 /* al click su next visualizza l'immagine successiva */
-next.addEventListener ('click', function (){
+next.addEventListener('click', function () {
     console.log("ho cliccato su next");
-    if(currentImage < itemsAll.length - 1){
-        itemsAll[currentImage].classList.remove('active');
-        currentImage++;
-        itemsAll[currentImage].classList.add('active');
-    }
+    itemsAll[currentImage].classList.remove('active');
+    currentImage = (currentImage + 1) % itemsAll.length;
+    itemsAll[currentImage].classList.add('active');
 });
